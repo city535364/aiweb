13fee561711dddb618aa6ef8a078288ceae02397
city535364 <43735609+city535364@users.noreply.github.com>
2018-10-07 09:56:21 +0800

    t27
    cgrrw

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ This example uses promises to create the classifier
 const image = document.getElementById('image'); // The image we want to classify
 const result = document.getElementById('result'); // The result tag in the HTML
 const probability = document.getElementById('probability'); // The probability tag in the HTML
-
+load_pic();
 // Initialize the Image Classifier method with MobileNet
 ml5.imageClassifier('MobileNet')
   .then(classifier => classifier.predict(image))
@@ -28,10 +28,7 @@ ml5.imageClassifier('MobileNet')
 function load_pic(){
   fn = decodeURI(getQueryString("file"));
   url = "https://firebasestorage.googleapis.com/v0/b/fileupload-40884.appspot.com/o/" + fn + "?alt=media&token=decc6dbb-1ff8-4944-9b69-88465b65e92b"  
-  //url = "kitten.jpg"
-  	
-  img = createImg(url, imageReady);
-  img.size(400, 400);
+  image.src = url;
 }
 
 
